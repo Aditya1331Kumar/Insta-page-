b6332652b0816e47f4c82ba754309f00061992aa
Aditya_Kumar_Gupta <Aditya.133131.kumar@gmail.com>
2026-02-04 11:34:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  const menu = document.getElementById("menu");
+  menu.style.display =
+    menu.style.display === "flex" ? "none" : "flex";
+}
+
+function openWhatsApp() {
+  window.open("https://wa.me/918809499019", "_blank");
+}
